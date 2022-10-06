00eccfdadee08bafab61a6b52ee0a29fdd932adb
RuhaimHakeem <ruhaim860@gmail.com>
2022-10-06 22:04:43 +0530

    Day 3 -  Added Song Details Page

--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -17,7 +17,10 @@ export const shazamCoreApi = createApi({
     getTopCharts: builder.query({
       query: () => "/charts/world",
     }),
+    getSongDetails: builder.query({
+      query: ({ songid }) => `/tracks/details?track_id=${songid}`,
+    }),
   }),
 });
 
-export const { useGetTopChartsQuery } = shazamCoreApi;
+export const { useGetTopChartsQuery, useGetSongDetailsQuery } = shazamCoreApi;
