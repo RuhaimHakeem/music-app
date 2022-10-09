a8d562874b845384fa5fcbc958fffa54cb4d61b1
RuhaimHakeem <ruhaim860@gmail.com>
2022-10-09 14:26:56 +0530

    Day-5 Completed

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -3,27 +3,40 @@ import { genres } from "../assets/constants";
 
 import { useDispatch, useSelector } from "react-redux";
 
-import { useGetTopChartsQuery } from "../redux/services/shazamCore";
+import {
+  useGetTopChartsQuery,
+  useGetSongsByGenreQuery,
+} from "../redux/services/shazamCore";
+import { selectGenreListId } from "../redux/features/playerSlice";
 
 const Discover = () => {
   const dispatch = useDispatch();
-  const { activeSong, isPlaying } = useSelector((state) => state.player);
-  const { data, isFetching, error } = useGetTopChartsQuery();
-  const genreTitle = "Pop";
+  const { activeSong, isPlaying, genreListId } = useSelector(
+    (state) => state.player
+  );
+  const { data, isFetching, error } = useGetSongsByGenreQuery(
+    genreListId || "POP"
+  );
 
   if (isFetching) return <Loader title="Loading Songs" />;
 
   if (error) return <Error />;
+
+  const genreTitle = genres.find(({ value }) => {
+    return value === genreListId;
+  });
   return (
     <div className="flex flex-col">
       <div className="w-full flex justify-between items-center sm:flex-row flex-col mt-4 mb-10">
         <h2 className="font-bold text-3xl text-white text-left">
-          Discover {genreTitle}
+          Discover {genreTitle?.title}
         </h2>
         <select
           className="bg-black text-gray-300 p-3 text-sm rounded-lg outline-none sm:mt-0 mt-5"
-          onChange={() => {}}
-          value=""
+          onChange={(e) => {
+            dispatch(selectGenreListId(e.target.value));
+          }}
+          value={genreListId || "POP"}
           name=""
           id=""
         >
